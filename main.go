217c1e2d6fd98fc8aafcc275d44a6cfebbc2ad50
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-08-15 17:29:03 +0700

    feat: add database service

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/NicholasLiem/GoLang_Microservice/database"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
@@ -14,9 +15,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	database.DB = database.SetupDB()
+
 	port := os.Getenv("PORT")
 
 	router := NewRouter()
+
+	log.Println("Running the server on port " + port)
 
 	log.Fatal(http.ListenAndServe("127.0.0.1:"+port, router))
 }
